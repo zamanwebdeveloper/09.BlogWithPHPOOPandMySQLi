42a378c456b737532464df32b1a90eeda702c6bc
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2018-12-25 15:39:30 +0600

    7/62 Blog With PHP OOP (Showing Post Details) : Part-06 of Blog With PHP OOP (Bangla Tutorial) Program of First Review Immediately Completed.

--- config/config.php
+++ config/config.php
@@ -3,4 +3,3 @@ define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "db_zamanwebdeveloper");
-
--- post.php
+++ post.php
@@ -1,26 +1,51 @@
 <?php include 'inc/header.php'?>
+<?php
+    if (!isset($_GET['id']) || $_GET['id'] == null ){
+        header("Location: 404.php");
+    }else{
+        $id = $_GET['id'];
+    }
+?>
 
 	<div class="contentsection contemplete clear">
 		<div class="maincontent clear">
 			<div class="about">
-				<h2>Our post title here</h2>
-				<h4>April 10, 2016, 12:30 PM, By Delowar</h4>
-				<img src="images/post2.png" alt="MyImage"/>
-				<p>Our psot..Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.</p>
-				
-				<p>About me..Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.</p>
+<?php
+			$query = "select * from tbl_post where id=$id";
+			$post = $db->select($query);
+            if ($post){
+            	while ($result=$post->fetch_assoc()){
 
-				<p>About me..Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.</p>
+?>
+				<h2><?php echo $result['title'];?></h2>
+                    <h4><?php echo $fm->formateDate($result['date']);?>, By <a href="#"><?php echo $result['author'];?></a></h4>
+                    <img src="admin/upload/<?php echo $result['image'];?>" alt="post image"/>
+                    <?php echo $result['body'];?>
 				
 				<div class="relatedpost clear">
 					<h2>Related articles</h2>
-					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+                    <?php
+                        $catId = $result['cat'];
+                        $queryReleted = "select * from tbl_post where cat='$catId' order by rand() limit 6";
+                        $postReleted = $db->select($queryReleted);
+                        if ($postReleted) {
+                            while ($resultReleted = $postReleted->fetch_assoc()) {
+                                ?>
+                                <a href="post.php?id=<?php echo $resultReleted['id'];?>">
+                                    <img src="admin/upload/<?php echo $resultReleted['image'];?>" alt="post image"/>
+                                </a>
+                                <?php
+                            }
+                        }else{
+                            echo "No Releted Post Available!!!";
+                        }
+                     ?>
 				</div>
+                <?php } ?>
+                <?php  }else{ header("Location: 404.php");
+                	}
+                ?>
+
 	</div>
 
 		</div>
